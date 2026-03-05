448991e437629b035868d75aa165d9e41e76fe52
MarcosAntonioFerreira <marcosantonioalmeida655@gmail.com>
2026-03-04 23:58:49 -0300

    feat: implementando o header-scedule

--- src/core-components/header-left.tsx
+++ src/core-components/header-left.tsx
@@ -1,18 +1,16 @@
-import Container from "../components/container";
+
 import Text from "../components/text";
-import FormSchedule from "./form-schedule";
+
 
 export default function HeaderLeft() {
     return (
-        <Container >
+        <>
             <Text variant={"title-lg-bold"} className="text-gray-100 leading-8" >
                 Agende um atendimento
             </Text>
             <Text variant={"text-sm-regular"} className="text-gray-300 mt-4 leading-5" >
                 Selecione data, horário e informe o nome do cliente para criar o agendamento
             </Text>
-            <FormSchedule />
-            
-        </Container>
+        </>
     )
 }
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,11 +1,23 @@
 import Container from "../components/container";
+import { HeaderItemSchedule } from "../components/header-item-schedule";
+import FormSchedule from "../core-components/form-schedule";
 import HeaderLeft from "../core-components/header-left";
+import SunHorizon from "../assets/icons/SunHorizon.svg?react"
 
 export default function PageHome() {
     return (
-        <>
-            <HeaderLeft />
-
-        </>
+        <div className="flex flex-row">
+            <Container>
+                <HeaderLeft />
+                <FormSchedule />
+            </Container>
+            <div className="bg-yellow w-full pl-28 pr-28">
+                <HeaderItemSchedule
+                    icon={SunHorizon}
+                    content_title={"Manhã"}
+                    content_schedules={"09h-12h"}
+                />
+            </div>
+        </div>
     )
 }
